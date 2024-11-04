f81452c0ea47b0300c3a184351fccdf309811b67
KEYBOARDYst <KEYBOARDYst@users.noreply.github.com>
2024-11-04 06:26:10 +0000

    deploy: e6cdef036b4be087baa1987e0423931b5e208171

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 console.log("config.js");
 const config = {
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: 'AIzaSyCb-QYB5R3FliwVc83ZebW00cBWgiKRsCE',
+    authDomain: 'keyboardyst-57662.firebaseapp.com',
+    projectId: 'keyboardyst-57662',
+    storageBucket: 'keyboardyst-57662.appspot.com',
+    messagingSenderId: '918205709481',
+    appId: '1:918205709481:web:c05268524bf62ed3b8fdb8',
     measurementId: '',
   },
 };
